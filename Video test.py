8dc2a208bf772abca5e59691e68dd105364a160a
Martyn0324 <giovanni.ab@outlook.com>
2021-06-30 18:39:18 -0300

    Add files via upload
    Trying to dismount videos into frames so machine learning models can be fitted and trained using them as a base.

---
+++ Video test.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+from sklearn.tree import DecisionTreeRegressor
+import imageio
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
+
+data = imageio.get_reader(r"C:\Users\giova\Videos\Captures\Teste.mp4", 'ffmpeg')
+model = TSNE()
+k = KMeans()
+
+for i, g in enumerate(data):
+    #print(g)
+    #plt.imshow(g)
+    #plt.show(block=False)
+    #tree = DecisionTreeRegressor()
+    #X1, X2, y1, y2 = train_test_split(i, g, test_size=0.3)
+    #tree.fit(X1, y1)
+    k.fit(g.reshape(-1,1))
+
+#Attention: DO NOT TRY the following:
+#Data = pd.DataFrame({'Frames': enumerate(data)})
